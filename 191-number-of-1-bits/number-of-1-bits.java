1ea1b78fdaebbf41e2c190e534b00d8840aa1756
Karthick-2307 <abineshkarthick03@gmail.com>
2026-06-22 13:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (80.61%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int hammingWeight(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) == 1) {
+                res += 1;
+            }
+        }
+        return res;        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

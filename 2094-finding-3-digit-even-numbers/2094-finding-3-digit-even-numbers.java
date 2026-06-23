7439b9985ef2b964135f24e679ad323afbedc004
Karthick-2307 <abineshkarthick03@gmail.com>
2026-06-23 21:51:20 +0530

    Time: 6 ms (82.84%), Space: 46.7 MB (62.87%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        int[] count = new int[10];
+        for (int d : digits) {
+            count[d]++;
+        }       
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 100; i <= 999; i += 2) {
+            int hundreds = i / 100;
+            int tens = (i / 10) % 10;
+            int ones = i % 10;
+            count[hundreds]--;
+            count[tens]--;
+            count[ones]--;
+            if (count[hundreds] >= 0 && count[tens] >= 0 && count[ones] >= 0) {
+                resultList.add(i);
+            }
+            count[hundreds]++;
+            count[tens]++;
+            count[ones]++;
+        }
+        int[] result = new int[resultList.size()];
+        for (int k = 0; k < resultList.size(); k++) {
+            result[k] = resultList.get(k);
+        }
+        
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
